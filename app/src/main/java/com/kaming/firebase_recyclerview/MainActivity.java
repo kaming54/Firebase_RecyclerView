2b12459228a634dc4b59c82866d4d34120f320b8
kaming54 <km20170101@gmail.com>
2019-03-27 09:40:21 +0800

    1

--- app/src/main/java/com/kaming/firebase_recyclerview/MainActivity.java
+++ app/src/main/java/com/kaming/firebase_recyclerview/MainActivity.java
@@ -3,7 +3,7 @@ package com.kaming.firebase_recyclerview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FirebaseAuth{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
